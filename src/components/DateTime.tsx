7b424f3acee1129ffda1946d85360b9e0cf2bbec
Dlaller <dlaller1988@gmail.com>
2024-06-07 18:08:43 +0530

    feat: enable disable logs task

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { getCurrentTime } from './GetCurrentTime';
 
 const DateTime = () => {
   const [currentTime, setCurrentTime] = useState('');
-
-
   useEffect(() => {
-    setCurrentTime(getCurrentTime());
+    const getCurrentTime = () => {
+      const now = new Date();
+      const formattedDate = `${now.getDate().toString().padStart(2, '0')}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getFullYear()}`;
+      const formattedTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
+      return `${formattedDate} : ${formattedTime}`;
+    };
+
     console.log(getCurrentTime())
+
+    setCurrentTime(getCurrentTime());
+
     const intervalId = setInterval(() => {
       setCurrentTime(getCurrentTime());
     }, 1000);
--- src/components/GetCurrentTime.ts
+++
@@ -1,6 +0,0 @@
-export const getCurrentTime = () => {
-    const now = new Date();
-    const formattedDate = `${now.getDate().toString().padStart(2, '0')}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getFullYear()}`;
-    const formattedTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
-    return `${formattedDate} : ${formattedTime}`;
-  };
